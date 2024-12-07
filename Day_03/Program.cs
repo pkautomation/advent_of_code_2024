d077af34c6568713d0c2cc77a1f0c46d5c8743d4
Pawel Kreski <pawel.kreski@hyland.com>
2024-12-07 10:24:15 +0100

    day4

---
+++ Day_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using Shared;
+using System.Text.RegularExpressions;
+
+var puzzleLines = FileReader.ReadFile("puzzle_03.txt");
+
+string myInput = @"mul[(](\d+),(\d+)[)]";
+
+var globalScore = 0;
+
+foreach (var item in puzzleLines)
+{
+    var matches = Regex.Matches(item, myInput);
+
+    foreach (Match match in matches)
+    {
+        int a = int.Parse(match.Groups[1].Value);
+        int b = int.Parse(match.Groups[2].Value);
+
+        globalScore += a * b;
+
+    }
+}
+
+Console.WriteLine(globalScore);
---
+++ Day_04/Program.cs
@@ -0,0 +1,9 @@
+﻿using Shared;
+
+var puzzleLines = FileReader.ReadFile("puzzle_04.txt");
+
+// czy jestem x
+// czy mam w lewo miejsce na MAS
+// sprawdz czy jest MAS w lewo
+//analogicznie w kazdy kierunek(8 kierunkow łącznie)
+//zsumuj wynik
